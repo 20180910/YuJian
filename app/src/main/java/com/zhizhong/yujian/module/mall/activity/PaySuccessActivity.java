0d6f49f1d73cda51f5d87d8fecc177d0891b4982
2380253499@qq.com <2380253499@qq.com>
2018-07-20 18:09:18 +0800

    文件

---
+++ app/src/main/java/com/zhizhong/yujian/module/mall/activity/PaySuccessActivity.java
@@ -0,0 +1,28 @@
+package com.zhizhong.yujian.module.mall.activity;
+
+import android.view.View;
+
+import com.zhizhong.yujian.base.BaseActivity;
+
+public class PaySuccessActivity extends BaseActivity {
+    @Override
+    protected int getContentView() {
+        setAppTitle("支付成功");
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void onViewClick(View v) {
+
+    }
+}
